347321d590ba4eb554a74289dcb35847085103d6
zcummings <kickasstimus@neo.tamu.edu>
2013-01-25 14:48:07 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaskNinja::Application.config.secret_token = '86c7d8be6ddf6f2a2c180c156f34ce4ab302120cc4cf321a54c949c1bba5b5c464ff083cbd1ce31a3582387e33e03a140af827db097791b6564630757077f3a6'
